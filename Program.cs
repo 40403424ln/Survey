effac862c83adbdd319d797744ac75836fb276fd
40403424ln <46713668+40403424ln@users.noreply.github.com>
2019-04-05 16:38:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+namespace Survey
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://deadwoodapitest.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Survey survey = new Survey
+                {
+                    enviroGroup = false,
+                    performedSurveyBefore = false,
+                    woodlandName = "Sherwood Forrest",
+                    latitude = "99",
+                    longitude = "99",
+                    gridRef = "5",
+                    woodlandSize = 5.55,
+                    deadwoodStandingOrLying = "Standing",
+                    deadwoodDiameter = 6.0,
+                    deadwoodLenght = 15.5,
+                    deadwoodSpecies = "Birch",
+                    holesInDeadwood = "yes",
+                    floraOnDeadwood = "More Moss",
+                    faunaOnDeadwood = "Bugs",
+                    pencilDepth = 5.0,
+                    deadwoodSoftness = "Hard",
+                    faunaInDeadwood = "More Bugs",
+                    deadwoodColour = "Derp Red",
+                    stateOfDecay = 3
+                };
+
+                var url = await CreateSurveyAsync(survey);
+                Console.WriteLine($"Create at {url}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task<Uri> CreateSurveyAsync(Survey survey)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/Surveys", survey);
+            response.EnsureSuccessStatusCode();
+            //Console.WriteLine(response.RequestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content.ToString());
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Survey.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    class Survey
+    {
+        public int Id { get; set; }
+        public bool enviroGroup { get; set; }
+        public bool performedSurveyBefore { get; set; }
+        public string woodlandName { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string gridRef { get; set; }
+        public double woodlandSize { get; set; }
+        public string deadwoodStandingOrLying { get; set; }
+        public double deadwoodDiameter { get; set; }
+        public double deadwoodLenght { get; set; }
+        public string deadwoodSpecies { get; set; }
+        public string holesInDeadwood { get; set; }
+        public string floraOnDeadwood { get; set; }
+        public string faunaOnDeadwood { get; set; }
+        public double pencilDepth { get; set; }
+        public string deadwoodSoftness { get; set; }
+        public string faunaInDeadwood { get; set; }
+        public string deadwoodColour { get; set; }
+        public int stateOfDecay { get; set; }
+    }
+}
